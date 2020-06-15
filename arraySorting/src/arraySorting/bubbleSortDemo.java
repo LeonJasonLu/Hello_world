b9cb94a65ebfc07fd2db7130eada6dcbe9d0c76b
LeonJasonLu <jason1997.0102@gmail.com>
2020-06-15 22:57:17 +0800

    Establish sorting project and add bubbleSort

---
+++ arraySorting/src/arraySorting/bubbleSortDemo.java
@@ -0,0 +1,28 @@
+package arraySorting;
+
+import java.util.Arrays;
+
+public class bubbleSortDemo {
+	static int[] a = {5,4,9,7,3,4,1,0,8};
+
+	public static void main(String[] args) {
+		bubbleSort(a);
+		System.out.print(Arrays.toString(a));
+
+	}
+	public static void bubbleSort(int[] a) {
+		for(int i = a.length - 1; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
+				if(a[j] > a[j+1]) {
+					swap(a, j, j+1);
+				}
+			}
+		}
+	}
+	private static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+}
